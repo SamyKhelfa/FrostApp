5763db72f174ccd55118ee30362d7c79437fb41d
Samy Khelfa <samy.khelfa94@gmail.com>
2023-11-24 09:59:54 +0100

    updates Courses Screen

--- containers/CoursesScreen.js
+++ containers/CoursesScreen.js
@@ -1,91 +1,69 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   ScrollView,
   Text,
   TouchableOpacity,
   StyleSheet,
   View,
-  Button,
 } from "react-native";
 import VimeoVideo from "./VimeoVideo";
 
-// Simulate data that might come from an API
-const courseData = [
-  {
-    moduleId: "1",
-    title: "🎯 Module 1: Vision, objectif et plan d'action sur-mesure",
-    lessons: [
-      {
-        title: "Introduction",
-        vimeoId: "872525023",
-      },
-      {
-        title: "Clarification de la vision avec le froid",
-        vimeoId: "872527912",
-      },
-      {
-        title: "Ton nouvel objectif avec la douche froide",
-        vimeoId: "872530511",
-      },
-      {
-        title: "3 méthodes pour prendre des douches froides",
-        vimeoId: "872531319",
-      },
-      {
-        title: "Construction de ton plan d’action sur-mesure",
-        vimeoId: "872532012",
-      },
-      { title: "L'état d'esprit indispensable pour toi", vimeoId: "872533537" },
-    ],
-  },
-  {
-    moduleId: "2",
-    title: "🧊 Module 2 : 7 techniques pour améliorer ton rapport au froid",
-    lessons: [
-      {
-        title: "Comment bien respirer sous tes douches froides",
-        vimeoId: "872534368",
-      },
-      {
-        title:
-          "Tips simples et puissants pour te sentir plus à l'aise avec le froid",
-        vimeoId: "872535440",
-      },
-    ],
-  },
-  {
-    moduleId: "3",
-    title: "🗓️ Module 3 : Routine solide et régularité",
-    lessons: [
-      {
-        title:
-          "Le best moment de la journée pour tes douches froides (pour toi)",
-        vimeoId: "872536608",
-      },
-      {
-        title: "Faut-il arrêter les douches chaudes ?",
-        vimeoId: "872538611",
-      },
-      {
-        title: "Faire de la douche froide une habitude quotidienne et solide",
-        vimeoId: "872539834",
-      },
-      {
-        title: "La règle d'or pour être régulier(ère)",
-        vimeoId: "872540578",
-      },
-      {
-        title:
-          "Réduire ta procrastination pour passer à l’action plus facilement",
-        vimeoId: "872541362",
-      },
-      {
-        title: "L'outil magique pour booster ta régularité et ta satisfaction",
-        vimeoId: "872542849",
-      },
-    ],
-  },
-];
+const CoursesScreen = () => {
+  const [courseData, setCourseData] = useState([]);
+  const [selectedLesson, setSelectedLesson] = useState(null);
+  const [selectedModuleId, setSelectedModuleId] = useState(null);
+  const [selectedLessonIndex, setSelectedLessonIndex] = useState(null);
+  const [isCoursesVisible, setIsCoursesVisible] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://192.168.0.222:3000/courses");
+        setCourseData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleLessonSelect = (moduleId, lessonIndex) => {
+    const selectedModule = courseData.find(
+      (module) => module.moduleId === moduleId
+    );
+    setSelectedLesson(selectedModule.lessons[lessonIndex]);
+    setSelectedModuleId(moduleId);
+    setSelectedLessonIndex(lessonIndex);
+    setIsCoursesVisible(false);
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={() => setIsCoursesVisible(!isCoursesVisible)}
+      >
+        <Text style={styles.buttonText}>
+          {isCoursesVisible ? "Cacher les Cours" : "Afficher les Cours"}
+        </Text>
+      </TouchableOpacity>
+      {isCoursesVisible &&
+        courseData.map((module, index) => (
+          <Module
+            key={module.moduleId}
+            module={module}
+            onLessonSelect={handleLessonSelect}
+            isVisible={isCoursesVisible}
+            selectedModuleId={selectedModuleId}
+            selectedLessonIndex={selectedLessonIndex}
+          />
+        ))}
+      {selectedLesson && <VimeoVideo vimeoId={selectedLesson.vimeoId} />}
+    </ScrollView>
+  );
+};
 
 const Module = ({
   module,
@@ -134,48 +112,6 @@ const Module = ({
           ))}
       </View>
     )
-  );
-};
-
-const CoursesScreen = () => {
-  const [selectedLesson, setSelectedLesson] = useState(null);
-  const [selectedModuleId, setSelectedModuleId] = useState(null);
-  const [selectedLessonIndex, setSelectedLessonIndex] = useState(null);
-  const [isCoursesVisible, setIsCoursesVisible] = useState(true);
-
-  const handleLessonSelect = (moduleId, lessonIndex) => {
-    const selectedModule = courseData.find(
-      (module) => module.moduleId === moduleId
-    );
-    setSelectedLesson(selectedModule.lessons[lessonIndex]);
-    setSelectedModuleId(moduleId);
-    setSelectedLessonIndex(lessonIndex);
-    setIsCoursesVisible(false);
-  };
-
-  return (
-    <ScrollView style={styles.container}>
-      <TouchableOpacity
-        style={styles.buttonStyle}
-        onPress={() => setIsCoursesVisible(!isCoursesVisible)}
-      >
-        <Text style={styles.buttonText}>
-          {isCoursesVisible ? "Cacher les Cours" : "Afficher les Cours"}
-        </Text>
-      </TouchableOpacity>
-      {isCoursesVisible &&
-        courseData.map((module) => (
-          <Module
-            key={module.moduleId}
-            module={module}
-            onLessonSelect={handleLessonSelect}
-            isVisible={isCoursesVisible}
-            selectedModuleId={selectedModuleId}
-            selectedLessonIndex={selectedLessonIndex}
-          />
-        ))}
-      {selectedLesson && <VimeoVideo vimeoId={selectedLesson.vimeoId} />}
-    </ScrollView>
   );
 };
 
