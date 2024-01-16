f9cd139072169c68bde6cf00cfe47900be9d033d
samy <samy@gmail.com>
2024-01-16 10:23:40 +0100

    Edit SignUpScreen

--- containers/SignUpScreen.js
+++ containers/SignUpScreen.js
@@ -8,31 +8,17 @@ import {
   Alert,
 } from "react-native";
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SignUpScreen = ({ navigation }) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isRegistered, setIsRegistered] = useState(false);
-
-  const [firstNameError, setFirstNameError] = useState(false);
-  const [lastNameError, setLastNameError] = useState(false);
-  const [emailError, setEmailError] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
-
-  const validateFields = () => {
-    setFirstNameError(!firstName.trim());
-    setLastNameError(!lastName.trim());
-    setEmailError(!email.trim());
-    setPasswordError(!password.trim());
-    return (
-      firstName.trim() && lastName.trim() && email.trim() && password.trim()
-    );
-  };
 
   const handleSignUp = async () => {
-    if (!validateFields()) {
+    if (!firstName || !lastName || !email || !password) {
+      Alert.alert("Erreur", "Tous les champs sont requis.");
       return;
     }
 
@@ -46,79 +32,57 @@ const SignUpScreen = ({ navigation }) => {
           password,
         }
       );
-      console.log(response.data);
-      setIsRegistered(true);
 
-      setTimeout(() => {
-        navigation.navigate("Profile");
-      }, 3000);
+      // Stockage du token et de l'ID de l'utilisateur dans AsyncStorage
+      await AsyncStorage.setItem("userToken", response.data.token);
+      await AsyncStorage.setItem("userId", response.data._id);
+
+      // Redirection vers la page de profil
+      navigation.navigate("Profile");
     } catch (error) {
       console.error(error);
-      if (error.response && error.response.status === 409) {
-        Alert.alert("Erreur", "Cet e-mail est déjà utilisé.");
-      } else {
-        Alert.alert(
-          "Erreur",
-          "Une erreur s'est produite lors de l'inscription."
-        );
+      let message = "Une erreur s'est produite lors de l'inscription.";
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        message = error.response.data.message;
       }
+      Alert.alert("Erreur", message);
     }
   };
 
   return (
     <View style={styles.container}>
-      {isRegistered ? (
-        <Text style={styles.successMessage}>
-          Inscription réussie! Redirection vers votre profil...
-        </Text>
-      ) : (
-        <>
-          {firstNameError && (
-            <Text style={styles.errorMessage}>Le prénom est requis.</Text>
-          )}
-          <TextInput
-            placeholder="Prénom"
-            value={firstName}
-            onChangeText={setFirstName}
-            style={[styles.input, firstNameError && styles.errorInput]}
-          />
-
-          {lastNameError && (
-            <Text style={styles.errorMessage}>Le nom est requis.</Text>
-          )}
-          <TextInput
-            placeholder="Nom"
-            value={lastName}
-            onChangeText={setLastName}
-            style={[styles.input, lastNameError && styles.errorInput]}
-          />
-
-          {emailError && (
-            <Text style={styles.errorMessage}>L'email est requis.</Text>
-          )}
-          <TextInput
-            placeholder="Email"
-            value={email}
-            onChangeText={setEmail}
-            style={[styles.input, emailError && styles.errorInput]}
-          />
-
-          {passwordError && (
-            <Text style={styles.errorMessage}>Le mot de passe est requis.</Text>
-          )}
-          <TextInput
-            placeholder="Mot de passe"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-            style={[styles.input, passwordError && styles.errorInput]}
-          />
-
-          <TouchableOpacity style={styles.button} onPress={handleSignUp}>
-            <Text style={styles.buttonText}>S'inscrire</Text>
-          </TouchableOpacity>
-        </>
-      )}
+      <TextInput
+        placeholder="Prénom"
+        value={firstName}
+        onChangeText={setFirstName}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Nom"
+        value={lastName}
+        onChangeText={setLastName}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Mot de passe"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.input}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <Text style={styles.buttonText}>S'inscrire</Text>
+      </TouchableOpacity>
     </View>
   );
 };
